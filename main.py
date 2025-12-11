ae1de7ba847571b81ef35fbb42febe285163f0a9
Zanolini <andrezano@icloud.com>
2025-12-11 10:22:36 +0100

    feat: enable book path input via command-line argument, updating main and stats functions to accept the path.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from stats import get_num_words, get_num_characters, sort_by_number 
+import sys
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     print("============ BOOKBOT ============")
-    get_num_words()
-    get_num_characters()
-    sort_by_number()
+    get_num_words(sys.argv[1])
+    get_num_characters(sys.argv[1])
+    sort_by_number(sys.argv[1])
 
 main()
--- stats.py
+++ stats.py
@@ -3,16 +3,16 @@ def get_book_text(path):
     with open(path, encoding="utf-8") as file:
         return file.read()
 
-def get_num_words():
-    book_text = get_book_text("books/frankenstein.txt")
+def get_num_words(path):
+    book_text = get_book_text(path)
     num_words = 0
     words = book_text.split()
     num_words = len(words)
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
 
-def get_num_characters():
-    book_text = get_book_text("books/frankenstein.txt")
+def get_num_characters(path):
+    book_text = get_book_text(path)
     num_characters = {}
     for char in book_text:
         lowered = char.lower()
@@ -22,9 +22,9 @@ def get_num_characters():
             num_characters[lowered] = 1
     return num_characters
 
-def sort_by_number():
+def sort_by_number(path):
     print("----------- Character Count ----------")
-    num_characters = get_num_characters()
+    num_characters = get_num_characters(path)
     sorted_tuples = sorted(num_characters.items(), key=lambda x: x[1], reverse=True)
     sorted_characters = [{"char": char, "num": count} for char, count in sorted_tuples]
     for item in sorted_characters:
