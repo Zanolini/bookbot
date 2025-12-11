8177e1fe9bf089358b5d07620f4e7502b54d0499
Zanolini <andrezano@icloud.com>
2025-12-11 10:13:34 +0100

    feat: enhance book analysis with sorted, case-insensitive character counts and improved output messages.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from stats import get_num_words, get_num_characters
+from stats import get_num_words, get_num_characters, sort_by_number 
 
 def main():
+    print("============ BOOKBOT ============")
     get_num_words()
     get_num_characters()
+    sort_by_number()
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,5 @@
 def get_book_text(path):
+    print(f"Analyzing book found at {path}...")
     with open(path, encoding="utf-8") as file:
         return file.read()
 
@@ -7,14 +8,24 @@ def get_num_words():
     num_words = 0
     words = book_text.split()
     num_words = len(words)
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
 
 def get_num_characters():
     book_text = get_book_text("books/frankenstein.txt")
     num_characters = {}
     for char in book_text:
-        if char in num_characters:
-            num_characters[char] += 1
-        else:
-            num_characters[char] = 1
-    print(num_characters)
+        lowered = char.lower()
+        if lowered in num_characters:
+            num_characters[lowered] += 1
+        elif lowered.isalpha():
+            num_characters[lowered] = 1
+    return num_characters
+
+def sort_by_number():
+    print("----------- Character Count ----------")
+    num_characters = get_num_characters()
+    sorted_tuples = sorted(num_characters.items(), key=lambda x: x[1], reverse=True)
+    sorted_characters = [{"char": char, "num": count} for char, count in sorted_tuples]
+    for item in sorted_characters:
+        print(f"{item['char']}: {item['num']}")
